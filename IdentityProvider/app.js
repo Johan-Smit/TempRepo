f00a3e1775d2e5a2d4cece286a089ff4a457bb6b
Johan Smit <johansm@bbd.co.za>
2023-06-24 22:39:44 +0200

    Other headers

--- IdentityProvider/app.js
+++ IdentityProvider/app.js
@@ -12,23 +12,23 @@ const App = express();
 
 App.use(express.json());
 App.use(cookieParser());
-// App.use((req, res, next) => {
-//     const allowedOrigins = ["http://guessthesong.us-east-1.elasticbeanstalk.com", "http://localhost:8080"]
-//     if (allowedOrigins.includes(req.headers.origin)) {
-//         console.log(req.headers.origin);
-//         res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-//    }
-//     res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-//     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//     res.setHeader("Access-Control-Allow-Credentials", true);
-//     next();
-//   });
+App.use((req, res, next) => {
+    const allowedOrigins = ["http://guessthesong.us-east-1.elasticbeanstalk.com", "http://localhost:8080"]
+    if (allowedOrigins.includes(req.headers.origin)) {
+        console.log(req.headers.origin);
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+   }
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  });
 
-  App.use(cors({
-    origin: process.env.ALLOWED_ORIGIN,
-    preflightContinue: true,
-    credentials: true,
-  }));
+  // App.use(cors({
+  //   origin: process.env.ALLOWED_ORIGIN,
+  //   preflightContinue: true,
+  //   credentials: true,
+  // }));
 
 
 const Port = process.env.PORT || 8081;
