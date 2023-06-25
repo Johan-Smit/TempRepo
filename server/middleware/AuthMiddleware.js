4053c90ea2e6ad9a5f282340579b7e19944a4469
Johan Smit <johansm@bbd.co.za>
2023-06-25 13:21:27 +0200

    logs

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -9,12 +9,16 @@ const verifyRequest = async (req, res, next) => {
     }
 
     if ((!req.cookies.token || !req.cookies.user)) {
+        console.log("no cookies or token");
+        console.log(req.cookies.token);
+        console.log(req.cookies.user);
         res.redirect("/authenticate");
     }
     else {
 
         const verificationResult = await authenticationController.verifyToken(req.cookies.user, req.cookies.token);
-        //console.log(verificationResult);
+        console.log("verify result");
+        console.log(verificationResult);
 
         if (verificationResult.data.message == "Success") {
             next();
